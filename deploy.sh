4cc648f8301837d8d0be2b351f9f33f2cdf29028
Zoe Rorvig <zrorvig@gmail.com>
2023-02-06 10:45:19 -0500

    made deploy.sh executable

--- deploy.sh
+++ deploy.sh
@@ -20,10 +20,7 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# if you are deploying to https://ZoeRorvig.github.io/react-portfolio
+git push -f git@github.com:ZoeRorvig/react-portfolio.git main:gh-pages
 
 cd -
